7c6b4cee831d8cd098a556c30c3aca03d817482f
Adrian Ramos <adrian.ramos.gonzalez@estudiantat.upc.edu>
2024-07-23 15:25:11 +0200

    trying to configure properly the script

--- batch.sh
+++ batch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #SBATCH -p gpi.compute             # Partition to submit to
-#SBATCH --mem=1G      # Max CPU Memory
-#SBATCH --gres=gpu:1
+#SBATCH --mem=64G      # Max CPU Memory
+#SBATCH --gres=gpu:2
 python xgoost_train_models.py
