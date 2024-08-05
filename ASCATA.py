9920aab759a322ee2c6cd75abefb6d4249aac06b
Adrian Ramos <adrian.ramos.gonzalez@estudiantat.upc.edu>
2024-08-05 08:35:58 +0200

    Update the ASCATA library with the uNET generators

--- ASCATA.py
+++ ASCATA.py
@@ -3,9 +3,9 @@ import numpy as np
 from netCDF4 import Dataset
 from collections.abc import Iterable
 import os
-
-class InputGenerator:
-    def __init__(self, input_var_names, target_var_names, input_grid='l2', input_dir=None,
+ 
+class uNETInputGenerator:
+    def __init__(self, input_var_names, target_var_names, input_grid='l3', input_dir=None,
                  output_dir=None, file_list=None, downsample_ratio=1, records_per_file=1e9, out_file_prefix="", seed=None, n_sigma=None):
         self.input_dir = input_dir
         self.output_dir = output_dir
@@ -13,7 +13,7 @@ class InputGenerator:
                                     'model': ['eastward_model_wind', 'northward_model_wind']}
         self.input_var_names = input_var_names
         self.target_var_names = target_var_names
-        self.separator = '\\'
+        self.separator = '' # Noise. It was \\ (Incorrect). / is correct for unix, but is redundant
         self.downsample_ratio = downsample_ratio
         self.n_sigma = n_sigma
         self.seed = seed
@@ -28,7 +28,7 @@ class InputGenerator:
             self.nc_reader = self.read_l3_nc_data
         else:
             self.nc_reader = self.read_l2_nc_data
-    def create_dir(self, dir):
+    def create_dir(self, dir): # sudo chmod a+rw /path/to/folder/that/you/want/to/write/to 
         if not os.path.exists(dir):
             os.makedirs(dir)
 
@@ -59,7 +59,9 @@ class InputGenerator:
         f = Dataset(fpath)
         selected_data = []
         lon = f.variables['lon'].__array__()
+        #lon = lon[2568:2824] # Eugenia 31/07 mail
         lat = f.variables['lat'].__array__()
+        #lat = lat[864:1120] # Eugenia 31/07 mail
         lats_mesh, lons_mesh = np.meshgrid(lat, lon, indexing='ij')
         for idx, var in enumerate(var_names):
             print(idx, var)
@@ -77,7 +79,7 @@ class InputGenerator:
         f.close()
         data = np.ma.stack(selected_data)
         return data
-
+    
     def calculate_target(self, fpath: str):
         scat_data = self.nc_reader(fpath, self.scat_model_var_names['scat'])
         model_data = self.nc_reader(fpath, self.scat_model_var_names['model'])
@@ -109,7 +111,7 @@ class InputGenerator:
         input_data = input_data.transpose()
         targets = targets.transpose()
         return input_data, targets
-
+    
     def generate_np_files(self):
         self.create_dir(self.output_dir)
         accumulated_inputs = []
